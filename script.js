0a76d79e3f0b8c3af3a5e752bf389538f266c4f8
Cristian Custodio <cristian325@gmail.com>
2020-01-16 15:43:03 -0500

    Syntax Correction Changes

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
-    function getIt(max,min)
-    
-    var characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz$^*?<>#@!%&()/";
-    var length = Math.floor(Math.random()*(max-min + 1))+ min;
-    var pwRandom = Array(length).fill(characters).map(function(execute)
-    {
-        return execute [Math.floor(Math.random()* execute.length)]
-    }).join('');
-    
-    return pwRandom;
-    
-    document.getElementById("generate").addEventListener("click",function()
-    {
-        pwRandom = getIt(128,8);
-        document.getElementById("randomPw").value=pwRandom;
-    });
+document.getElementById("generate").addEventListener("click", function() {
+  console.log("Hello World!");
+  pwRandom = getIt(128, 8);
+  console.log(pwRandom);
+  document.getElementById("randomPw").value = pwRandom;
+});
 
+function getIt(max, min) {
+  var characters =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz$^*?<>#@!%&()/";
+  var length = Math.floor(Math.random() * (max - min + 1)) + min;
+  var pwRandom = Array(length)
+    .fill(characters)
+    .map(function(execute) {
+      return execute[Math.floor(Math.random() * execute.length)];
+    })
+    .join("");
 
-
-
-
-
-
-
-
-
-
-
-
-    
+  return pwRandom;
+}
